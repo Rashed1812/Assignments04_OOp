d2219edbbacb172ca416fa63492490cbeb561936
Your Name <you@example.com>
2025-01-20 18:53:04 +0200

    Answer Part01 Q2

--- Assignments04_OOp/Program.cs
+++ Assignments04_OOp/Program.cs
@@ -11,8 +11,21 @@
             //Console.WriteLine(cal1.Add(1, 2));
             //Console.WriteLine(cal1.Add(1, 2, 3));
             //Console.WriteLine(cal1.Add(1.5, 2.5)); 
-            
+
             #endregion
+
+            #region 2.Create a class named Rectangle with the following constructors:
+
+            //Rectangle rec1 = new Rectangle();
+            //Rectangle rec2 = new Rectangle(5, 10);
+            //Rectangle rec3 = new Rectangle(30);
+
+            //Console.WriteLine($"Rectangle one: Width = {rec1.Width}, Height = {rec1.Height}");
+            //Console.WriteLine($"Rectangle Two: Width = {rec2.Width}, Height = {rec2.Height}");
+            //Console.WriteLine($"Rectangle Three: Width = {rec3.Width}, Height = {rec3.Height}"); 
+
+            #endregion
+
 
         }
     }
---
+++ Assignments04_OOp/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments04_OOp
+{
+    internal class Rectangle
+    {
+        #region 2.Create a class named Rectangle with the following constructors:
+
+        //public int Width { get; set; }
+        //public int Height { get; set; }
+
+        //public Rectangle()
+        //{
+        //    Width = 0;
+        //    Height = 0;
+        //}
+        //public Rectangle(int width, int height)
+        //{
+        //    Width = width;
+        //    Height = height;
+        //}
+        //public Rectangle(int value)
+        //{
+        //    Width = value;
+        //    Height = value;
+        //} 
+
+        #endregion
+
+    }
+}
