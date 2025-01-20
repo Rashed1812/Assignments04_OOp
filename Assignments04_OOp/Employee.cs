7d66b7b336e9f2a89cdb5d2304cfddaa09457597
Your Name <you@example.com>
2025-01-20 19:14:58 +0200

    Answer Part01 Q4

---
+++ Assignments04_OOp/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments04_OOp
+{
+    internal class Employee
+    {
+        #region 4.Create a derived class named Manager that overrides the Work method to print "Manager is managing". 
+        
+        //public virtual void Work()
+        //{
+        //    Console.WriteLine("Employee is working");
+        //} 
+
+        #endregion
+    }
+}
---
+++ Assignments04_OOp/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments04_OOp
+{
+    internal class Manager : Employee
+    {
+
+        #region 4.Create a derived class named Manager that overrides the Work method to print "Manager is managing". 
+        
+        //public override void Work()
+        //{
+        //    base.Work();
+        //    Console.WriteLine("Manager is managing");
+        //} 
+
+        #endregion
+
+
+    }
+}
